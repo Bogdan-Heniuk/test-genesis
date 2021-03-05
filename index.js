5c4b1d2f898ceed8d457fcf894954da9870a16fe
poponchik <you@example.com>
2021-03-05 19:19:58 +0200

    add knexfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended :true}))
 app.use(cors())
 
 app.get('/', (req, res) => {
